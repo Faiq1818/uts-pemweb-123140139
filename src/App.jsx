68df4c4565c2435ba3974b586c0794f3716fb313
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-11-01 19:42:57 +0700

    installing react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Routes, Route } from "react-router";
+import Root from "./pages/root";
+
 export default function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Root />} />
+    </Routes>
   )
 }
 
